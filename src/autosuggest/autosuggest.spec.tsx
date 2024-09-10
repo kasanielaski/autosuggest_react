27183c0d1fce307a8ebd95e55744bb654d6da0ca
Sergey Shapirenko <sergey.shapirenko@invent.us>
2024-09-10 15:21:09 +0700

    add test

---
+++ src/autosuggest/autosuggest.spec.tsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import Autosuggest from "./autosuggest";
+
+describe('Autosuggest component', () => {
+  it('should render', () => {
+    expect(Autosuggest). toBeDefined();
+  })
+})
